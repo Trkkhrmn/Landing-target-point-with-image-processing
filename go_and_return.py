b062ed6b31ed80a04316e610018ff5be647c2388
Tarık KAHRAMAN <93274619+Trkkhrmn@users.noreply.github.com>
2024-02-11 11:19:43 +0300

    Update go_and_return.py

--- go_and_return.py
+++ go_and_return.py
@@ -5,6 +5,44 @@ import time
 from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative
 from pymavlink import mavutil 
 
+def disarm(wait=True, timeout=None):
+    print("Disarming")
+    vehicle.armed = False
+    if wait:
+        while vehicle.armed:
+            time.sleep(1)
+            if timeout is not None and timeout <= 0:
+                break
+            if timeout is not None:
+                timeout -= 1
+
+def goto_position_target_global_int(aLocation):
+    msg = vehicle.message_factory.set_position_target_global_int_encode(
+        0, 0, 0,    
+        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT_INT, 
+        0b0000111111111000, 
+        aLocation.lat*1e7, 
+        aLocation.lon*1e7, 
+        aLocation.alt, 
+        0, 0, 0, 
+        0, 0, 0, 
+        0, 0)     
+
+    vehicle.send_mavlink(msg)
+
+def send_body_ned_velocity(velocity_x, velocity_y, velocity_z, duration=0):
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+        0, 0, 0,    
+        mavutil.mavlink.MAV_FRAME_BODY_NED, 
+        0b0000111111000111, 
+        0, 0, 0, 
+        velocity_x, velocity_y, velocity_z, 
+        0, 0, 0, 
+        0, 0)
+    for x in range(0, duration):
+        vehicle.send_mavlink(msg)
+        time.sleep(1)
+
 def arm_and_takeoff(aTargetAltitude):
     print ("Basic pre-arm checks")
     while not vehicle.is_armable:
@@ -13,8 +51,8 @@ def arm_and_takeoff(aTargetAltitude):
 
     print ("Arming motors")
 
-    vehicle.mode    = VehicleMode("GUIDED")
-    vehicle.armed   = True
+    vehicle.mode = VehicleMode("GUIDED")
+    vehicle.armed = True
 
     while not vehicle.armed:
         print (" Waiting for arming...")
@@ -25,41 +63,9 @@ def arm_and_takeoff(aTargetAltitude):
     
     while True:
         print (" Altitude: "), vehicle.location.global_relative_frame.alt
-        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95:
+        if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
             print ("Reached target altitude")
             break
-        time.sleep(1)
-
-def goto_position_target_global_int(aLocation):
-    
-    msg = vehicle.message_factory.set_position_target_global_int_encode(
-        0,       
-        0, 0,    
-        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT_INT, 
-        0b0000111111111000, 
-        aLocation.lat*1e7, 
-        aLocation.lon*1e7, 
-        aLocation.alt, 
-        0, 
-        0, 
-        0, 
-        0, 0, 0, 
-        0, 0)     
-
-    vehicle.send_mavlink(msg)
-
-def send_body_ned_velocity(velocity_x, velocity_y, velocity_z, duration=0):
-    msg = vehicle.message_factory.set_position_target_local_ned_encode(
-        0,       
-        0, 0,    
-        mavutil.mavlink.MAV_FRAME_BODY_NED, 
-        0b0000111111000111, 
-        0, 0, 0, 
-        velocity_x, velocity_y, velocity_z, 
-        0, 0, 0, 
-        0, 0)
-    for x in range(0,duration):
-        vehicle.send_mavlink(msg)
         time.sleep(1)
 
 vehicle = connect('/dev/ttyS0', baud=921600)
@@ -80,7 +86,7 @@ cmds.wait_ready()
 
 print (" Home Location: %s" % vehicle.home_location)
 
-vehicle.home_location=vehicle.location.global_frame
+vehicle.home_location = vehicle.location.global_frame
 print (" New Home Location: %s" % vehicle.home_location)
 
 arm_and_takeoff(1)
@@ -95,5 +101,6 @@ vehicle.mode = VehicleMode("LAND")
 arm_and_takeoff(1)
 goto_position_target_global_int(vehicle.home_location)
 vehicle.mode = VehicleMode("LAND")
+
 disarm(wait=True, timeout=None)
 vehicle.close()
